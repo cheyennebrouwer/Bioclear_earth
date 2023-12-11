d8a14154bf0a167f356aeb7c03f273b160ad071f
cheyennebrouwer <e.h.b.brouwer@st.hanze.nl>
2023-12-11 11:44:27 +0100

    Added changes in EDA and bioclear.py

--- bioclear.py
+++ bioclear.py
@@ -1,16 +1,24 @@
 import pandas as pd
+import panel as pn
+import RUST as rust
+import taxonomy
 
 
-def read_fasta(file_path):
-    with open(file_path, "r") as file:
-        for line in file:
-            line = line.strip()
-            print(line)
+# def read_fasta(file_path):
+#     with open(file_path, "r") as file:
+#         for line in file:
+#             line = line.strip()
+#             print(line)
+#
+# # take one zotu and put in variable
+#
+# file_path_tax = "/Users/cheyennebrouwer/Documents/23-24/Kwartaal_2/Data_Dashboards/Bioclear/wetransfer_ngs-data_2023-11-14_1231/NGS data/ASV_taxonomy.txt"
+#
+# df_tax_file = pd.DataFrame()
+# read_fasta(file_path_tax)
 
+# Taxonomy.from_newick(variable that contains zotu)
 
-file_path_fasta = "/Bioclear/wetransfer_ngs-data_2023-11-14_1231/NGS data/ASV.fa"
-
-read_fasta(file_path_fasta)
-
-
+tax_file = pd.read_csv("/Users/cheyennebrouwer/Documents/23-24/Kwartaal_2/Data_Dashboards/Bioclear/wetransfer_ngs-data_2023-11-14_1231/NGS data/ASV_taxonomy.txt")
+one_zotu = tax_file.loc["Zotu1"]
 
